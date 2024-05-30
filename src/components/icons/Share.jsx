5b54f53b1f649f4e2f517de15f2f1a0f8e247752
Juan Morales <juanchito89768@gmail.com>
2024-05-30 16:50:07 -0500

    feat(Root): Added icons
    Similar icons to tiktok ui added

---
+++ src/components/icons/Share.jsx
@@ -0,0 +1,18 @@
+export const Share = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='35'
+      height='35'
+      viewBox='0 0 24 24'
+      strokeWidth='1.5'
+      stroke='currentColor'
+      fill='#fff'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <path d='M13 4v4c-6.575 1.028 -9.02 6.788 -10 12c-.037 .206 5.384 -5.962 10 -6v4l8 -7l-8 -7z' />
+    </svg>
+  )
+}
